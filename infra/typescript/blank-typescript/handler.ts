467f67f34d016a59ac45658ac3c1ab53a7df27bc
pelaakso <pelaakso@users.noreply.github.com>
2025-03-10 18:49:25 +0200

    Use JSON logging format for TS Lambda

--- infra/typescript/blank-typescript/handler.ts
+++ infra/typescript/blank-typescript/handler.ts
@@ -5,14 +5,14 @@ export const handler: Handler = async (
   context: Context,
 ): Promise<APIGatewayProxyResult> => {
   const input = typeof event === 'string' ? event : JSON.stringify(event);
-  console.log(`Lambda input event: ${input}`);
-  console.log(`Lambda input context: ${JSON.stringify(context)}`);
+  console.debug(`Lambda input event: ${input}`);
+  console.debug(`Lambda input context: ${JSON.stringify(context)}`);
 
-  console.log(`Function ${context.functionName}:${context.functionVersion} handler was called`);
+  console.info(`Function ${context.functionName}:${context.functionVersion} handler was called`);
 
   // Print out environment variables
-  console.log('Environment variables:');
-  console.log(JSON.stringify(process.env, null, 2));
+  console.debug('Environment variables:');
+  console.debug(JSON.stringify(process.env, null, 2));
 
   return {
     statusCode: 200,
